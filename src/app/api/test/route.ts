b78e967029a649989cc75dbca2aabc1087a57d7c
Shuffled720 <shuffled720@gmail.com>
2025-02-14 02:47:50 +0530

    get and post request

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -4,8 +4,15 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
 export async function POST(req: NextRequest) {
-    const data = await req.json();
-    return NextResponse.json({ message: 'Data received', data });
+    try{
+        const data = await req.json();
+
+        return NextResponse.json({ message: 'Data received', data });
+
+    }catch(e){
+        return NextResponse.json({ message: 'Error', error: e });
+    }
+    
 }
 
 export async function GET() {
