17a107f741d454af212cef187332463ace725aeb
Shuffled720 <shuffled720@gmail.com>
2025-02-15 11:26:18 +0530

    .evn next auth link added

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 const LogoutButton = () => {
   return (
     <button
-      onClick={() => signOut({ callbackUrl: "/login" })} // Redirects to login after logout
+      onClick={() => signOut()} // Redirects to login after logout
       className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
     >
       Logout
