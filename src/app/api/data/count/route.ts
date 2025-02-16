b86e86ebc2cdcd82df376205d6f0da02a0c688db
Rupal17shah <shah17rupal@gmail.com>
2025-02-16 22:44:18 +0530

    creates count route

---
+++ src/app/api/data/count/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import Student from "@/models/Student";
+import Professor from "@/models/Professor";
+import Project from "@/models/Project";
+import { dbConnect } from "@/lib/mongodb";
+
+export async function GET(req: NextRequest) {
+  try {
+    await dbConnect();
+    const students = await Student.countDocuments({});
+    const professors = await Professor.countDocuments({});
+    const projects = await Project.countDocuments({});
+
+    return NextResponse.json(
+      { message: "GET count request received", students, professors, projects },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
