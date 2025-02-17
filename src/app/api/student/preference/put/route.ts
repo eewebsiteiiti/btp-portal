fc75749739d29951badc2f3dc84d2f9a87842587
Shuffled720 <shuffled720@gmail.com>
2025-02-18 01:33:14 +0530

    fixes #1: added put route for student preference

--- src/app/api/student/preference/put/route.ts
+++ src/app/api/student/preference/put/route.ts
@@ -5,7 +5,7 @@ import { dbConnect } from "@/lib/mongodb";
 export async function PUT(req: NextRequest) {
   try {
     await dbConnect();
-    let data = await req.json();
+    const data = await req.json();
     const filter = { email: data.email, name: data.name };
     const update = { preference: data.preference };
     console.log(filter, update);
