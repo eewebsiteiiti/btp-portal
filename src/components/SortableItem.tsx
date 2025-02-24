2df5f83c98f4ce3a61826e143f294c4ff513690f
rupal17shah <shah17rupal@gmail.com>
2025-02-25 00:31:58 +0530

    adds the group input functionality

--- src/components/SortableItem.tsx
+++ src/components/SortableItem.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { useSortable } from "@dnd-kit/sortable";
+import { useState } from "react";
 import { CSS } from "@dnd-kit/utilities";
 import { Card } from "@/components/ui/card";
 import { SortableItemProps } from "@/types";
+import { Button } from "./ui/button";
 
 export default function SortableItem({ id, project, index, isOverlay }: SortableItemProps) {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
-
+  const [roll, setRoll] = useState("");
+  const [isRoll, setIsRoll] = useState(false);
   const style = {
     transform: CSS.Transform.toString(transform),
     transition: isOverlay ? "none" : transition || "transform 0.2s ease",
@@ -19,22 +22,42 @@ export default function SortableItem({ id, project, index, isOverlay }: Sortable
       style={style}
       {...attributes}
       {...listeners}
-      className={`cursor-grab active:cursor-grabbing p-4 rounded-xl border shadow-sm transition-all duration-200 
+      className={`cursor-grab active:cursor-grabbing justify-between p-4 rounded-xl border shadow-sm transition-all duration-200
         bg-background border-border ${isOverlay ? "opacity-50 scale-105" : ""} flex items-center gap-4`}
     >
-      <div className="text-sm font-semibold text-muted-foreground">{index}.</div>
-      <div className="flex flex-col">
-        <h3 className="text-base font-medium text-foreground">{project.Title}</h3>
-        <p className="text-xs text-muted-foreground">Supervisor: {project.Supervisor}</p>
-        {project.Cosupervisor && (
-          <p className="text-xs text-muted-foreground">Co-Supervisor: {project.Cosupervisor}</p>
-        )}
-        <p className="text-xs text-muted-foreground truncate">{project.Comments}</p>
-        <p className="text-xs text-muted-foreground truncate">Domain: {project.Domain}</p>
-        <p className="text-xs text-muted-foreground truncate">Project No: {project.Project_No}</p>
-        <p className="text-xs text-muted-foreground truncate">Nature of Work: {project.Nature_of_work}</p>
-        <p className="text-xs text-muted-foreground">Capacity: {project.Capacity}</p>
+      <div className="flex items-center justify-center gap-4">
+        <div className="text-sm font-semibold text-muted-foreground">{index}.</div>
+        <div className="flex flex-col">
+          <h3 className="text-base font-medium text-foreground">{project.Title}</h3>
+          <p className="text-xs text-muted-foreground">Supervisor: {project.Supervisor}</p>
+          {project.Cosupervisor && (
+            <p className="text-xs text-muted-foreground">Co-Supervisor: {project.Cosupervisor}</p>
+          )}
+          <p className="text-xs text-muted-foreground truncate">{project.Comments}</p>
+          <p className="text-xs text-muted-foreground truncate">Domain: {project.Domain}</p>
+          <p className="text-xs text-muted-foreground truncate">Project No: {project.Project_No}</p>
+          <p className="text-xs text-muted-foreground truncate">Nature of Work: {project.Nature_of_work}</p>
+          <p className="text-xs text-muted-foreground">Capacity: {project.Capacity}</p>
+          {isRoll && <p className="text-sm text-blue-800">Partner's Roll: {roll}</p>}
+        </div>
       </div>
+      {Number(project.Capacity) > 1 ? <>
+
+        <div className="flex items-center gap-4 justify-between">
+          <form>
+            <input
+              type="text"
+              onChange={(e) => setRoll(e.target.value)}
+              value={roll}
+              placeholder="Enter partner's roll number"
+              className="w-full p-3 border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition"
+            />
+
+          </form>
+          <Button className="ml-auto" size="sm" disabled={roll.length!==9} onClick={()=>{setIsRoll(true)}}>Add a partner</Button>
+        </div>
+
+      </> : <></>}
     </Card>
   );
 }
